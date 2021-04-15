a41e4649c6b903360844975df16e56546e6aacc7
BarbaraSheaKenny <b.kenny58@gmail.com>
2021-04-15 15:23:13 -0400

    backend working

--- Develop/server.js
+++ Develop/server.js
@@ -1,36 +1,41 @@
 // Dependencies
-
+// var uniqid = require('uniqid');
 const express = require('express');
 const path = require('path');
-
+let data = require('./db/db.json');
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'))
 
-// Data
 
-const notes = []
+//Routes
 
-// Starts server
+app.get('/api/notes', (req, res) => res.json(data));
 
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
-// Routes
+app.use(express.static('public'))
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
-
-app.get('/api/notes', (req, res) => res.json(notes));
 
 // Note Handler
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
     console.log(newNote);
   
-    notes.push(newNote);
+    data.push(newNote);
     res.json(newNote);
   });
   
+
+// Starts server
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+
+
+
+
